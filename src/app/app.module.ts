67319fe5f7b9a5dfcb6678dd5c3cffca173c39a8
matata samuel <mattasamuel3@gmail.com>
2020-11-16 14:47:48 +0300

    this commit will add a doctor login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DoctorLoginComponent } from './doctor-login/doctor-login.component';
+import { PatientSignupComponent } from './patient-signup/patient-signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoctorLoginComponent,
+    PatientSignupComponent
   ],
   imports: [
     BrowserModule,
