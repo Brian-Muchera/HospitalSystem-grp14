13acdc71dfba186f6721133d9df19b7203158e47
tonyishangu <tonyishangu@gmail.com>
2020-11-17 10:45:22 +0300

    create api service

--- src/app/records.service.ts
+++ src/app/records.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { from } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecordsService {
 
-  constructor() { }
+  private REST_API_SERVICE = ""
+
+  constructor(private httpClient: HttpClient) { }
 }
