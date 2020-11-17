db71ac91c1acb5e1a276cc7af4907a90a7d26554
Kate <you@example.com>
2020-11-17 15:12:57 +0300

    patients booking form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,54 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PatientComponent } from './patient/patient.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNavComponent } from './mat-nav/mat-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatTableComponent } from './mat-table/mat-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatBoardComponent } from './mat-board/mat-board.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { BookinFormComponent } from './booking/bookin-form/bookin-form.component';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PatientComponent
+    PatientComponent,
+    MatNavComponent,
+    MatTableComponent,
+    MatBoardComponent,
+    BookinFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+
+    FormsModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/booking/bookin-form/bookin-form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bookin-form',
+  templateUrl: './bookin-form.component.html',
+  styleUrls: ['./bookin-form.component.css']
+})
+export class BookinFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  
+}
