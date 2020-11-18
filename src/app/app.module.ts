d14bce884a2116b819509a9714092351f935dade
matata samuel <mattasamuel3@gmail.com>
2020-11-18 06:49:24 +0300

    this commit will add a registration form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DoctorLoginComponent } from './doctor-login/doctor-login.component';
 import { PatientSignupComponent } from './patient-signup/patient-signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DoctorLoginComponent,
+    AppComponent,   
     PatientSignupComponent
   ],
   imports: [
--- src/app/patient-signup/patient-signup.component.ts
+++ src/app/patient-signup/patient-signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+// import { first_name } from '../patientR/patient';
+
+// import { from } from 'rxjs';
 
 @Component({
   selector: 'app-patient-signup',
@@ -6,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-signup.component.css']
 })
 export class PatientSignupComponent implements OnInit {
+  
 
   constructor() { }
 
