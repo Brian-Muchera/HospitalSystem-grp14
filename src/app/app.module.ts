4a786521ba7f88637927df933e586442d282a298
Brian-Muchera <mucherabrian2@gmail.com>
2020-11-22 21:19:14 +0300

    added codes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    BoardAdminComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/profile.ts
@@ -0,0 +1,12 @@
+export class Profile {
+    id: number;
+    username: string;
+    accessToken: string;
+    bio: string;
+    address: string
+    phone: number
+
+   
+
+
+}
