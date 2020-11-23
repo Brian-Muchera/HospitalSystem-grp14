fbac4764dd007def5b635fcc2287d52e97ecf686
Brian-Muchera <mucherabrian2@gmail.com>
2020-11-23 18:08:50 +0300

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,29 +5,26 @@ import { DashboardloginComponent } from './dashboardlogin/dashboardlogin.compone
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { LoginBoardComponent } from './components/login-board/login-board.component';
+import { PatientLoginComponent } from './components/patient-login/patient-login.component';
+import { DoctorLoginComponent } from './components/doctor-login/doctor-login.component';
+import { UserBoardComponent } from './components/user-board/user-board.component';
+import { ProfileComponent } from './profile/profile.component';
+import { PatientSignupComponent } from './patient-signup/patient-signup.component'
+
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'services',component:AboutComponent},
   {path:'testimonials',component:TestimonialsComponent},
   {path:'dashboardlogin',component:DashboardloginComponent},
-
-import { LoginBoardComponent } from './components/login-board/login-board.component';
-import { PatientLoginComponent } from './components/patient-login/patient-login.component';
-import { DoctorLoginComponent } from './components/doctor-login/doctor-login.component';
-import { UserBoardComponent } from './components/user-board/user-board.component';
-
-
-const routes: Routes = [ 
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  //{ path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginBoardComponent },
   { path: 'login/patient', component: PatientLoginComponent },
   { path: 'login/doctor', component:  DoctorLoginComponent },
   { path: 'user', component:  UserBoardComponent },
-import { ProfileComponent } from './profile/profile.component';
-
-const routes: Routes = [
   { path: 'profile', component: ProfileComponent },
+  { path: 'register', component: PatientSignupComponent },
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,3 @@
-
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
@@ -7,12 +5,20 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ProfileComponent } from './profile/profile.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
 import { DashboardloginComponent } from './dashboardlogin/dashboardlogin.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginBoardComponent } from './components/login-board/login-board.component';
+import { PatientLoginComponent } from './components/patient-login/patient-login.component';
+import { DoctorLoginComponent } from './components/doctor-login/doctor-login.component';
+import { UserBoardComponent } from './components/user-board/user-board.component';
+import { DoctorsBoardComponent } from './components/doctors-board/doctors-board.component';
+import { PatientSignupComponent } from './patient-signup/patient-signup.component';
+import { from } from 'rxjs';
+
 
 
 @NgModule({
@@ -23,50 +29,21 @@ import { FooterComponent } from './footer/footer.component';
     TestimonialsComponent,
     DashboardloginComponent,
     FooterComponent,
-    
-
-import { LoginBoardComponent } from './components/login-board/login-board.component';
-import { PatientLoginComponent } from './components/patient-login/patient-login.component';
-import { DoctorLoginComponent } from './components/doctor-login/doctor-login.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { UserBoardComponent } from './components/user-board/user-board.component';
-import { DoctorsBoardComponent } from './components/doctors-board/doctors-board.component';
-
-
-
-import { PatientSignupComponent } from './patient-signup/patient-signup.component';
-import { from } from 'rxjs';
-
-@NgModule({
-  declarations: [
-
-    AppComponent,
     LoginBoardComponent,
     PatientLoginComponent,
     DoctorLoginComponent,
     UserBoardComponent,
-    DoctorsBoardComponent
-
-    AppComponent,   
-    PatientSignupComponent
-
-import { ProfileComponent } from './profile/profile.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
+    DoctorsBoardComponent, 
+    PatientSignupComponent,
     ProfileComponent,
 
+
+    
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-
-    FormsModule,
-    HttpClientModule
-
     HttpClientModule,
     FormsModule
 
